929e751034c4212bcca2a0037236d16ce6de5ef6
Nirupam Debnath <nirupamdebnath4@gmail.com>
2025-01-02 23:29:05 +0530

    svc-a Returns request headers,body and query string

--- src-a/src/index.ts
+++ src-a/src/index.ts
@@ -1,12 +1,18 @@
 import express, {Request, Response} from 'express';
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 3000;
+
+app.use(bodyParser.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
     status: 'Success',
     message: 'Response from src-a',
+    reqBody: req.body,
+    reqHeader: req.headers,
+    reqQuery: req.query
   });
 });
 
